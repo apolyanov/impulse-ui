a2b121db28ba4517df2418563f823d52a5446fb8
Alexander Polyanov <76997835+apolyanov@users.noreply.github.com>
2023-10-02 02:16:49 +0300

    Ci improvements (#3)
    Setting up CI for automatic publishing
    
    ---------
    
    Co-authored-by: Alexander Polyanov <alexander,polyanov@gmail.com>

--- src/augmentation/theme.d.ts
+++
@@ -1,8 +0,0 @@
-import 'styled-components';
-
-import { AppTheme } from '../components/theme/types/theme.types';
-
-declare module 'styled-components' {
-  // eslint-disable-next-line @typescript-eslint/no-empty-interface
-  export interface DefaultTheme extends AppTheme {}
-}
