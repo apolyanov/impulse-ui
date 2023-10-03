be8243763bf90f66c1c42be92649f27fb5167517
Alexander Polyanov <alexander,polyanov@gmail.com>
2023-10-03 09:51:20 +0300

    chore: Creating template package for future development

---
+++ scripts/createPackage.ts
@@ -0,0 +1,28 @@
+import { cpSync as copy, readFileSync, rmSync as remove, writeFileSync } from 'fs';
+
+const templateDir = './packages/template-package';
+const packageName = process.argv[2];
+const packageDir = `./packages/impulse-ui-${packageName}`;
+const nameArgRegex = /name-arg/gi;
+
+const copyTemplate = () => {
+  console.info('Copying template folder...');
+  copy(templateDir, packageDir, { recursive: true });
+
+  console.info('Cleaning node_modules...');
+  remove(`${packageDir}/node_modules`, { recursive: true, force: true });
+
+  console.info('Cleaning dist...');
+  remove(`${packageDir}/dist`, { recursive: true, force: true });
+
+  console.info('Reading package.json...');
+  let packageJson = readFileSync(`${packageDir}/package.json`, 'utf8');
+
+  console.info('Updating package.json...');
+  packageJson = packageJson.replace(nameArgRegex, packageName);
+
+  console.log('Saving package.json...');
+  writeFileSync(`${packageDir}/package.json`, packageJson);
+};
+
+copyTemplate();
