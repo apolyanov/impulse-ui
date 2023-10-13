415ca6e3e51a1743b050dc2251b0468dd0e4f8a7
apolyanov <alexander_1256@abv.bg>
2023-10-13 23:16:31 +0300

    feat: Modernizing styled function syntax.

--- packages/impulse-ui-buttons/src/components/link/BaseLink.styles.tsx
+++ packages/impulse-ui-buttons/src/components/link/BaseLink.styles.tsx
@@ -4,14 +4,14 @@ import styled, { css } from 'styled-components';
 
 import { link } from '../../styles';
 
-const BaseLink = styled.a<BaseIComponent<LinkStyleProps>>`
-  ${({ theme: { mode, themes }, $iStyle, $iProps, $parentProps }) => {
+const BaseLink = styled.a<BaseIComponent<LinkStyleProps>>(
+  ({ theme: { mode, themes }, $iStyle, $iProps, $parentProps }) => {
     const { iColorTheme, iCss } = mergeThemes(themes?.link?.iStyle ?? link.iStyle, $iStyle, $parentProps);
     const themeMode = getThemeMode(mode);
     const iProps = mergeProps(themes?.link?.iStyleProps ?? link.iStyleProps, $iProps);
 
     return css(iCss(iColorTheme[themeMode], iProps));
-  }}
-`;
+  },
+);
 
 export default BaseLink;
