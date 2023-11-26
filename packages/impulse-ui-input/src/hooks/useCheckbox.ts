69152cc1774f5376088ed5a948cf1616f286fa61
apolyanov <alexander_1256@abv.bg>
2023-11-26 04:47:20 +0200

    feat: Checkbox update to be controlled

--- packages/impulse-ui-input/src/hooks/useCheckbox.ts
+++ packages/impulse-ui-input/src/hooks/useCheckbox.ts
@@ -1,16 +1,19 @@
-import { useState } from 'react';
+import { MouseEvent, useState } from 'react';
 import { CompositeCheckboxProps } from '@impulse-ui/types';
 
 const useCheckbox = (rest: CompositeCheckboxProps) => {
-  const { checked, label, ...buttonProps } = rest;
+  const { checked, label, onClick, ...buttonProps } = rest;
 
   const [isChecked, setIsChecked] = useState<boolean>(Boolean(checked));
+  const getCheckedState = () => checked ?? isChecked;
 
-  const toggleCheckbox = () => {
-    setIsChecked((prevState) => !prevState);
+  const toggleCheckbox = (event: MouseEvent<HTMLButtonElement>) => {
+    const value = !getCheckedState();
+    onClick?.({ event, value });
+    setIsChecked(value);
   };
 
-  return { isChecked, label, buttonProps, toggleCheckbox };
+  return { isChecked, label, buttonProps, toggleCheckbox, getCheckedState, checked };
 };
 
 export { useCheckbox };
