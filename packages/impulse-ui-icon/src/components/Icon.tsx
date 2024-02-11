27f6f8250bb0042f36d2af373195998118d23ab5
apolyanov <alexander_1256@abv.bg>
2024-02-11 21:06:24 +0200

    feat: Change font weight and extend icon component types.

--- packages/impulse-ui-icon/src/components/Icon.tsx
+++ packages/impulse-ui-icon/src/components/Icon.tsx
@@ -4,8 +4,8 @@ import { IconProps } from '@impulse-ui/types';
 
 import BaseIcon from './BaseIcon.styles';
 
-const Icon: FunctionComponent<IconProps> = ({ icon, iStyle, iProps, parentProps }) => {
-  return <BaseIcon $parentProps={parentProps} icon={icon} $iStyle={iStyle} $iProps={iProps} />;
+const Icon: FunctionComponent<IconProps> = ({ icon, iStyle, iProps, parentProps, ...rest }) => {
+  return <BaseIcon $parentProps={parentProps} icon={icon} $iStyle={iStyle} $iProps={iProps} {...rest} />;
 };
 
 export { Icon };
--- packages/impulse-ui-types/src/components/icon/icon.types.ts
+++ packages/impulse-ui-types/src/components/icon/icon.types.ts
@@ -1,11 +1,12 @@
 import React from 'react';
 import { IconDefinition } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIconProps } from '@fortawesome/react-fontawesome';
 
 import { ComponentStyleProps, IComponent } from '../theme';
 
 type IconStyleProps = ComponentStyleProps;
 
-interface IconProps extends IComponent<IconStyleProps> {
+interface IconProps extends IComponent<IconStyleProps>, FontAwesomeIconProps {
   icon: IconDefinition;
   onClick?: (event: React.MouseEvent<SVGSVGElement, MouseEvent>) => void;
 }
