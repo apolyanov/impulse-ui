984f9b86c3d3ae46a039a90e87f9a9b216613669
apolyanov <alexander_1256@abv.bg>
2024-05-13 14:21:41 +0300

    fix: Fixing Button styles

--- example/components/pages/ButtonsPage.tsx
+++ example/components/pages/ButtonsPage.tsx
@@ -3,16 +3,33 @@ import { faArrowRightFromBracket } from '@fortawesome/free-solid-svg-icons';
 import { Button, IconButton, Link, TextButton } from '@impulse-ui/buttons';
 import { Checkbox } from '@impulse-ui/input';
 import { Container } from '@impulse-ui/layout';
+import { Typography } from '@mui/material';
 
 const ButtonsPage = () => {
   const [check, setCheck] = useState<boolean>(false);
 
   return (
     <Container>
+      <Typography>Button</Typography>
+      <Button onClick={() => console.log('clicked')}>Buttons</Button>
+      <Button loading onClick={() => console.log('clicked')}>
+        Buttons
+      </Button>
       <Button disabled onClick={() => console.log('clicked')}>
         Buttons
       </Button>
+      <Button disabled loading onClick={() => console.log('clicked')}>
+        Buttons
+      </Button>
+      <Typography>Text Button</Typography>
+      <TextButton>Hello</TextButton>
+      <TextButton loading disabled>
+        Hello
+      </TextButton>
       <TextButton disabled>Hello</TextButton>
+      <TextButton loading disabled>
+        Hello
+      </TextButton>
       <Link>Hello</Link>
       <Checkbox disabled checked={check} onClick={({ value }) => setCheck(value)} label={'Click me'} />
       <IconButton disabled icon={faArrowRightFromBracket} />
