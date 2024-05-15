0c0c0f44a8e5cca6e60368a1456f08cf53b958c6
apolyanov <alexander_1256@abv.bg>
2024-05-16 02:05:35 +0300

    fix: Missing getThemeColor function in BaseTypograhy component

--- packages/impulse-ui-text/src/components/typography/BaseTypography.styles.tsx
+++ packages/impulse-ui-text/src/components/typography/BaseTypography.styles.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { getThemeMode, mergeThemes } from '@impulse-ui/core';
+import { getThemeColor, getThemeMode, mergeThemes } from '@impulse-ui/core';
 import { BaseIComponent } from '@impulse-ui/types';
 import styled, { css } from 'styled-components';
 
@@ -12,7 +12,7 @@ const BaseTypography = styled.p<BaseIComponent>(({ theme: { mode, themes }, $iSt
   });
   const themeMode = getThemeMode(mode);
 
-  return css(iCss({ iColorTheme: iColorTheme[themeMode] }));
+  return css(iCss({ iColorTheme: iColorTheme[themeMode], getThemeColor: getThemeColor(iColorTheme[themeMode]) }));
 });
 
 export default BaseTypography;
