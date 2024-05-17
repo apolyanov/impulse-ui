1a80f9a853a8ea139ef1da2bc2d194dc9c3030c2
apolyanov <alexander_1256@abv.bg>
2024-05-18 01:15:23 +0300

    fix: Adding missing dependencies to toolkit package

--- example/components/pages/QRScannerPage.tsx
+++ example/components/pages/QRScannerPage.tsx
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Container } from '@impulse-ui/layout';
 import { QRScanner } from '@impulse-ui/qr';
+import { Typography } from '@impulse-ui/text';
+import { IScannerControls } from '@zxing/browser';
+import { Result } from '@zxing/library';
 
 const TablePage = () => {
+  const [result, setResult] = useState<Result>();
+  const [controls, setControls] = useState<IScannerControls>();
+
   return (
-    <Container iStyle={{ iCss: { display: 'flex', justifyContent: 'center' } }}>
-      <QRScanner />
+    <Container iStyle={{ iCss: { display: 'flex', alignItems: 'center', flexDirection: 'column', padding: 4 } }}>
+      <QRScanner
+        onDecode={(result, controls) => {
+          setResult(result);
+          setControls(controls);
+        }}
+      />
+      <Typography as='pre'>{JSON.stringify(result, null, 2)}</Typography>
+      <Typography as='pre'>{JSON.stringify(controls, null, 2)}</Typography>
     </Container>
   );
 };
