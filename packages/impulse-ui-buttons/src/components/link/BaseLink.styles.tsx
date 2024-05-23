2b766221fa592a3b041a25f76ec98660e4cf89e6
apolyanov <alexander_1256@abv.bg>
2024-05-23 15:09:15 +0300

    feat: Refactoring buttons package to use the generic createBaseComponentStyle function

--- packages/impulse-ui-buttons/src/components/link/BaseLink.styles.tsx
+++ packages/impulse-ui-buttons/src/components/link/BaseLink.styles.tsx
@@ -1,18 +1,17 @@
 'use client';
-import { getThemeColor, getThemeMode, mergeThemes } from '@impulse-ui/core';
+import { createBaseComponentStyle } from '@impulse-ui/core';
 import { BaseIComponent } from '@impulse-ui/types';
-import styled, { css } from 'styled-components';
+import styled from 'styled-components';
 
 import { link } from '../../styles';
 
-const BaseLink = styled.a<BaseIComponent>(({ theme: { mode, themes }, $iStyle }) => {
-  const { iColorTheme, iCss } = mergeThemes({
-    defaultTheme: themes?.link?.iStyle ?? link,
+const BaseLink = styled.a<BaseIComponent>(({ theme: { mode, themes }, $iStyle }) =>
+  createBaseComponentStyle({
+    baseTheme: link,
+    globalTheme: themes?.link?.iStyle,
     overridingTheme: $iStyle,
-  });
-  const themeMode = getThemeMode(mode);
-
-  return css(iCss({ iColorTheme: iColorTheme[themeMode], getThemeColor: getThemeColor(iColorTheme[themeMode]) }));
-});
+    mode,
+  }),
+);
 
 export { BaseLink };
