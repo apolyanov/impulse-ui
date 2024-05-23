105879a15f777ce4202cd6a35e5dab7a7645befa
apolyanov <alexander_1256@abv.bg>
2024-05-23 15:12:29 +0300

    feat: Refactoring input and layout packages to use the generic createBaseComponentStyle function

--- packages/impulse-ui-layout/src/components/divider/BaseDivider.styles.ts
+++ packages/impulse-ui-layout/src/components/divider/BaseDivider.styles.ts
@@ -1,18 +1,17 @@
 'use client';
-import { getThemeColor, getThemeMode, mergeThemes } from '@impulse-ui/core';
+import { createBaseComponentStyle } from '@impulse-ui/core';
 import { BaseIComponent } from '@impulse-ui/types';
-import styled, { css } from 'styled-components';
+import styled from 'styled-components';
 
 import { divider } from '../../styles';
 
-const BaseDivider = styled.div<BaseIComponent>(({ theme: { themes, mode }, $iStyle }) => {
-  const { iColorTheme, iCss } = mergeThemes({
-    defaultTheme: themes?.divider?.iStyle ?? divider,
+const BaseDivider = styled.div<BaseIComponent>(({ theme: { themes, mode }, $iStyle }) =>
+  createBaseComponentStyle({
+    baseTheme: divider,
+    globalTheme: themes?.divider?.iStyle,
     overridingTheme: $iStyle,
-  });
-  const themeMode = getThemeMode(mode);
-
-  return css(iCss({ iColorTheme: iColorTheme[themeMode], getThemeColor: getThemeColor(iColorTheme[themeMode]) }));
-});
+    mode,
+  }),
+);
 
 export default BaseDivider;
