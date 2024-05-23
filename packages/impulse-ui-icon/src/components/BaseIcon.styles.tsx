e818b8768e6bd3664c7e2747633cfe8ba9c51939
apolyanov <alexander_1256@abv.bg>
2024-05-23 15:10:38 +0300

    feat: Refactoring icon package to use the generic createBaseComponentStyle function

--- packages/impulse-ui-icon/src/components/BaseIcon.styles.tsx
+++ packages/impulse-ui-icon/src/components/BaseIcon.styles.tsx
@@ -1,19 +1,18 @@
 'use client';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { getThemeColor, getThemeMode, mergeThemes } from '@impulse-ui/core';
+import { createBaseComponentStyle } from '@impulse-ui/core';
 import { BaseIComponent } from '@impulse-ui/types';
-import styled, { css } from 'styled-components';
+import styled from 'styled-components';
 
 import { icon } from '../styles';
 
-const BaseIcon = styled(FontAwesomeIcon)<BaseIComponent>(({ theme: { mode, themes }, $iStyle }) => {
-  const { iColorTheme, iCss } = mergeThemes({
-    defaultTheme: themes?.icon?.iStyle ?? icon,
+const BaseIcon = styled(FontAwesomeIcon)<BaseIComponent>(({ theme: { mode, themes }, $iStyle }) =>
+  createBaseComponentStyle({
+    baseTheme: icon,
+    globalTheme: themes?.icon?.iStyle,
     overridingTheme: $iStyle,
-  });
-  const themeMode = getThemeMode(mode);
-
-  return css(iCss({ iColorTheme: iColorTheme[themeMode], getThemeColor: getThemeColor(iColorTheme[themeMode]) }));
-});
+    mode,
+  }),
+);
 
 export default BaseIcon;
