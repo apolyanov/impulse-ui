392faa7dc7f2d647af20e15f54f67f5e0374adb7
apolyanov <alexander_1256@abv.bg>
2024-05-25 01:22:13 +0300

    fix: Adding types to the components that will support direct css props

--- packages/impulse-ui-types/src/components/loaders/spinner.types.ts
+++ packages/impulse-ui-types/src/components/loaders/spinner.types.ts
@@ -1,5 +1,23 @@
-import { IComponent } from '../theme';
+import {
+  BordersCssProps,
+  DisplayCssProps,
+  FlexboxCssProps,
+  GridCssProps,
+  IComponent,
+  PositionsCssProps,
+  SizingCssProps,
+  SpacingAliasCssProps,
+  SpacingCssProps,
+} from '../theme';
 
-type SpinnerLoaderProps = IComponent;
+type SpinnerLoaderProps = IComponent &
+  Partial<SpacingCssProps> &
+  Partial<SpacingAliasCssProps> &
+  Partial<BordersCssProps> &
+  Partial<FlexboxCssProps> &
+  Partial<GridCssProps> &
+  Partial<PositionsCssProps> &
+  Partial<SizingCssProps> &
+  Partial<DisplayCssProps>;
 
 export type { SpinnerLoaderProps };
