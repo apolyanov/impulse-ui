64d9b675d807622041b9f83d5f06651b630e208a
apolyanov <alexander_1256@abv.bg>
2024-05-25 00:20:59 +0300

    feat: Adding spacing props logic in the core package

---
+++ packages/impulse-ui-core/src/utils/transforms.ts
@@ -0,0 +1,25 @@
+import { SpacingAliases } from '@impulse-ui/types';
+
+import { spacingAliasesMap } from './constants';
+
+const borderTransform = (value: string | number) => {
+  if (typeof value !== 'number') {
+    return value;
+  }
+
+  return `${value}px solid`;
+};
+
+const spacingTransform = (value: string | number, alias: SpacingAliases) => {
+  const spacingProps = spacingAliasesMap[alias];
+
+  if (Array.isArray(spacingProps)) {
+    return spacingProps.map((propName) => [propName, value]);
+  }
+
+  return value;
+};
+
+const noTransform = (value: string | number) => value;
+
+export { borderTransform, noTransform, spacingTransform };
