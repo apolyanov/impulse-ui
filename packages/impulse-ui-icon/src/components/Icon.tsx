067b3a5234762445fdcf4e25a979926b2a39f324
apolyanov <alexander_1256@abv.bg>
2024-05-25 00:24:55 +0300

    feat: Adding spacing props logic in the icon package

--- packages/impulse-ui-icon/src/components/Icon.tsx
+++ packages/impulse-ui-icon/src/components/Icon.tsx
@@ -1,11 +1,12 @@
 'use client';
 import React, { FunctionComponent } from 'react';
+import { extractCssProps } from '@impulse-ui/core';
 import { IconProps } from '@impulse-ui/types';
 
 import BaseIcon from './BaseIcon.styles';
 
 const Icon: FunctionComponent<IconProps> = ({ icon, iStyle, ...rest }) => {
-  return <BaseIcon icon={icon} $iStyle={iStyle} {...rest} />;
+  return <BaseIcon icon={icon} $iStyle={iStyle} $cssProps={extractCssProps(rest)} {...rest} />;
 };
 
 export { Icon };
