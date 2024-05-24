e3da03dd9135d624e8c65a61ea1f01f34dcb0034
apolyanov <alexander_1256@abv.bg>
2024-05-25 00:28:39 +0300

    feat: Adding spacing props logic in the layout package

--- packages/impulse-ui-layout/src/components/divider/Divider.tsx
+++ packages/impulse-ui-layout/src/components/divider/Divider.tsx
@@ -1,12 +1,13 @@
 'use client';
 import React from 'react';
 import { FunctionComponent } from 'react';
+import { extractCssProps } from '@impulse-ui/core';
 import { DividerProps } from '@impulse-ui/types';
 
 import BaseDivider from './BaseDivider.styles';
 
 const Divider: FunctionComponent<DividerProps> = ({ iStyle, ...rest }) => {
-  return <BaseDivider $iStyle={iStyle} {...rest} />;
+  return <BaseDivider $iStyle={iStyle} $cssProps={extractCssProps(rest)} {...rest} />;
 };
 
 export { Divider };
