89c76e9fc8fb7ef03b97d6c03aca9b36f7730cfb
apolyanov <alexander_1256@abv.bg>
2024-05-25 00:28:39 +0300

    feat: Adding spacing props logic in the layout package

--- packages/impulse-ui-layout/src/components/divider/BaseDivider.styles.ts
+++ packages/impulse-ui-layout/src/components/divider/BaseDivider.styles.ts
@@ -5,12 +5,13 @@ import styled from 'styled-components';
 
 import { divider } from '../../styles';
 
-const BaseDivider = styled.div<BaseIComponent>(({ theme: { themes, mode }, $iStyle }) =>
+const BaseDivider = styled.div<BaseIComponent>(({ theme: { themes, mode }, $iStyle, ...rest }) =>
   createBaseComponentStyle({
     baseTheme: divider,
     globalTheme: themes?.divider?.iStyle,
     overridingTheme: $iStyle,
     mode,
+    rest,
   }),
 );
 
