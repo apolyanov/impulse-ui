72328317d83921f3a270086ed13b319a71c572d9
apolyanov <alexander_1256@abv.bg>
2024-07-11 23:49:46 +0300

    Initial move of the old qr scanner

---
+++ packages/impulse-ui-qr/src/hooks/useQrScanner.ts
@@ -0,0 +1,65 @@
+import { useCallback, useRef, useState } from 'react';
+import { NotFoundException } from '@impulse-ui/scanner-core/src';
+import { CameraCapabilities, QRScannerRestProps } from '@impulse-ui/types';
+
+import { Scanner } from '../scanner';
+
+const decode = (image: Uint8ClampedArray, width: number, height: number) => {
+  const code = jsQR(image, width, height);
+
+  if (code) return code;
+
+  throw new NotFoundException();
+};
+
+const useQrScanner = (options: QRScannerRestProps) => {
+  const videoElement = useRef<HTMLVideoElement | null>(null);
+  const qrScanner = useRef(new Scanner(options.scanningInterval));
+
+  const [cameraCapabilities, setCameraCapabilities] = useState<CameraCapabilities>();
+  const [isScanning, setIsScanning] = useState<boolean>(false);
+  const [isTorchOn, setIsTorchOn] = useState<boolean>(false);
+
+  const toggleScanning = async () => {
+    if (!isScanning && videoElement.current) {
+      qrScanner.current.scan(videoElement.current, options.onSuccess, options.onError).then((camera) => {
+        setCameraCapabilities(camera);
+        setIsScanning(true);
+      });
+    } else {
+      stopScanning();
+    }
+  };
+
+  const stopScanning = () => {
+    qrScanner.current.stop();
+    setIsScanning(false);
+  };
+
+  const toggleTorch = () => {
+    const newTorchState = !isTorchOn;
+
+    qrScanner.current.videoContext.toggleTorch().then(() => setIsTorchOn(newTorchState));
+  };
+
+  const canUseTorch = cameraCapabilities?.torch;
+
+  const mountVideoElement = useCallback((element: HTMLVideoElement | null) => {
+    if (!element) qrScanner.current.stop();
+
+    videoElement.current = element;
+  }, []);
+
+  return {
+    videoElement,
+    toggleScanning,
+    toggleTorch,
+    cameraCapabilities,
+    canUseTorch,
+    isScanning,
+    isTorchOn,
+    mountVideoElement,
+  };
+};
+
+export { useQrScanner };
