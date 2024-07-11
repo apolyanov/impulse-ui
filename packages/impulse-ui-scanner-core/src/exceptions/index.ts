136b3c7c850643c8f07f70258ad4b54484cf0449
apolyanov <alexander_1256@abv.bg>
2024-07-11 23:49:46 +0300

    Initial move of the old qr scanner

--- packages/impulse-ui-scanner-core/src/exceptions/index.ts
+++ packages/impulse-ui-scanner-core/src/exceptions/index.ts
@@ -3,5 +3,7 @@ export * from './NoCameraCapabilitiesAvailableException';
 export * from './NoCanvasContextException';
 export * from './NoCurrentStreamException';
 export * from './NoMediaTrackException';
+export * from './NoOnSuccessCallbackException';
+export * from './NoScanningFunctionException';
 export * from './NotFoundException';
 export * from './NoVideoElementException';
--- packages/impulse-ui-scanner-core/src/index.ts
+++ packages/impulse-ui-scanner-core/src/index.ts
@@ -1,1 +1,3 @@
-export const template = 'You take it from here';
+export * from './exceptions';
+export * from './hooks';
+export * from './scanner';
