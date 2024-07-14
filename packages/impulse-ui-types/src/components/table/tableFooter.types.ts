82ab70058922a2a6be0d6e5a3699622094aa1762
apolyanov <alexander_1256@abv.bg>
2024-07-14 03:24:56 +0300

    fix: Adding lodash/merge as external.

--- packages/impulse-ui-types/src/components/table/tableFooter.types.ts
+++ packages/impulse-ui-types/src/components/table/tableFooter.types.ts
@@ -1,10 +1,7 @@
-import { Table } from '@tanstack/react-table';
-
 import { ContainerHTMLProps } from '../layout';
 import { TypographyHTMLProps } from '../text';
 import { IOStyle } from '../theme';
 
-import { ImpulseTableProviderUtils } from './impulseTableProvider.types';
 import { PaginationStyle } from './pagination.types';
 
 interface TableFooterProps<T = object> {
