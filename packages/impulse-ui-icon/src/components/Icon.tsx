3da38aba7f87c0f2373592f4e0bc1b1deaa031f8
apolyanov <alexander.polyanov@gmail.com>
2024-09-28 14:03:57 +0300

    fix: Fixing external libraries list

--- packages/impulse-ui-icon/src/components/Icon.tsx
+++ packages/impulse-ui-icon/src/components/Icon.tsx
@@ -3,8 +3,6 @@ import { FunctionComponent } from 'react';
 import { IconProps } from '../types';
 import { BaseIcon } from '../components';
 
-import '@fortawesome/fontawesome-svg-core/styles.css';
-
 const Icon: FunctionComponent<IconProps> = ({ icon, iStyle, ...rest }) => {
   return <BaseIcon icon={icon} $iStyle={iStyle} $cssProps={extractCssProps(rest)} {...rest} />;
 };
