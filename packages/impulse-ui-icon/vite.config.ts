0238ecf75cac3a9ae5b9344ed3b1383bc8ecb870
apolyanov <alexander.polyanov@gmail.com>
2024-09-29 01:47:07 +0300

    fix: Fixing bundles names and toolkit directives

--- packages/impulse-ui-icon/vite.config.ts
+++ packages/impulse-ui-icon/vite.config.ts
@@ -1,4 +1,4 @@
 import { defineConfig } from 'vite';
 import { baseConfig } from '../../vite.base.config';
 
-export default defineConfig(baseConfig('impulse-ui-icon'));
+export default defineConfig(baseConfig('icon'));
