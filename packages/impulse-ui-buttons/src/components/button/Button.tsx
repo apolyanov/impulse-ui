829006967cba45bdadb4c4d47fcd2001a02a4f33
apolyanov <alexander.polyanov@gmail.com>
2024-09-30 15:56:44 +0300

    feat: Adding polymorphic properties to all button components

--- packages/impulse-ui-buttons/src/components/button/Button.tsx
+++ packages/impulse-ui-buttons/src/components/button/Button.tsx
@@ -1,6 +1,6 @@
-import { extractCssProps, useComponentStyle } from '@impulse-ui/core';
+import { extractCssProps, polymorphicForwardRef, useComponentStyle } from '@impulse-ui/core';
 import { Spinner } from '@impulse-ui/loader';
-import { FunctionComponent } from 'react';
+import { PropsWithChildren } from 'react';
 import { ButtonProps } from '../../types';
 
 import { buttonComponentMap } from '../../maps';
@@ -8,15 +8,15 @@ import { button } from '../../styles';
 
 import { BaseButton } from './BaseButton.styles';
 
-const Button: FunctionComponent<ButtonProps> = ({ iStyle, ...rest }) => {
+const Button = polymorphicForwardRef<'button', PropsWithChildren<ButtonProps>>(({ iStyle, ...rest }, ref) => {
   const { children, loading, ...buttonProps } = rest;
   const { buttonStyle, loaderStyle } = useComponentStyle(buttonComponentMap, rest, button, iStyle);
 
   return (
-    <BaseButton $iStyle={buttonStyle} $cssProps={extractCssProps(buttonProps)} {...buttonProps}>
+    <BaseButton ref={ref} $iStyle={buttonStyle} $cssProps={extractCssProps(buttonProps)} {...buttonProps}>
       {loading ? <Spinner data-disabled={buttonProps.disabled} iStyle={loaderStyle} /> : children}
     </BaseButton>
   );
-};
+});
 
 export { Button };
--- vite/src/components/pages/ComponentStyle.tsx
+++ vite/src/components/pages/ComponentStyle.tsx
@@ -1,7 +1,14 @@
-import { Container } from '@impulse-ui/layout';
+import { Container } from "@impulse-ui/layout";
+import { Button } from "@impulse-ui/buttons";
 
 const ComponentStyle = () => {
-  return <Container>Styles page</Container>;
+  return (
+    <Container>
+      <Button as="a" href={"LINK"}>
+        Test
+      </Button>
+    </Container>
+  );
 };
 
 export default ComponentStyle;
