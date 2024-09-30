f0839b12082418a1337dd7ce448276ce6460cb7d
apolyanov <alexander.polyanov@gmail.com>
2024-09-30 15:56:44 +0300

    feat: Adding polymorphic properties to all button components

--- packages/impulse-ui-buttons/src/components/icon-button/IconButton.tsx
+++ packages/impulse-ui-buttons/src/components/icon-button/IconButton.tsx
@@ -1,14 +1,14 @@
-import { extractCssProps, useComponentStyle } from '@impulse-ui/core';
+import { extractCssProps, polymorphicForwardRef, useComponentStyle } from '@impulse-ui/core';
 import { Icon } from '@impulse-ui/icon';
 import { Spinner } from '@impulse-ui/loader';
-import { FunctionComponent } from 'react';
+import { PropsWithChildren } from 'react';
 import { IconButtonProps } from '../../types';
 
 import { iconButtonComponentMap } from '../../maps';
 import { defaultIconButtonStyle } from '../../styles';
 import { BaseButton } from '../button';
 
-const IconButton: FunctionComponent<IconButtonProps> = ({ iStyle, ...rest }) => {
+const IconButton = polymorphicForwardRef<'button', PropsWithChildren<IconButtonProps>>(({ iStyle, ...rest }, ref) => {
   const { icon, loading, ...buttonProps } = rest;
 
   const { buttonStyle, iconStyle, loaderStyle } = useComponentStyle(
@@ -19,7 +19,7 @@ const IconButton: FunctionComponent<IconButtonProps> = ({ iStyle, ...rest }) => 
   );
 
   return (
-    <BaseButton $iStyle={buttonStyle} $cssProps={extractCssProps(buttonProps)} {...buttonProps}>
+    <BaseButton ref={ref} $iStyle={buttonStyle} $cssProps={extractCssProps(buttonProps)} {...buttonProps}>
       {loading ? (
         <Spinner data-disabled={buttonProps.disabled} iStyle={loaderStyle} />
       ) : (
@@ -27,6 +27,6 @@ const IconButton: FunctionComponent<IconButtonProps> = ({ iStyle, ...rest }) => 
       )}
     </BaseButton>
   );
-};
+});
 
 export { IconButton };
--- packages/impulse-ui-buttons/src/components/link/Link.tsx
+++ packages/impulse-ui-buttons/src/components/link/Link.tsx
@@ -1,11 +1,11 @@
-import { extractCssProps } from '@impulse-ui/core';
-import { FunctionComponent } from 'react';
+import { extractCssProps, polymorphicForwardRef } from '@impulse-ui/core';
+import { PropsWithChildren } from 'react';
 import { LinkProps } from '../../types';
 
 import { BaseLink } from './BaseLink.styles';
 
-const Link: FunctionComponent<LinkProps> = ({ iStyle, ...rest }) => {
-  return <BaseLink $iStyle={iStyle} $cssProps={extractCssProps(rest)} {...rest} />;
-};
+const Link = polymorphicForwardRef<'a', PropsWithChildren<LinkProps>>(({ iStyle, ...rest }, ref) => {
+  return <BaseLink ref={ref} $iStyle={iStyle} $cssProps={extractCssProps(rest)} {...rest} />;
+});
 
 export { Link };
