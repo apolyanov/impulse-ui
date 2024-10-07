17eebf806a22d07f72d28d6ec074fb8d457507e9
apolyanov <alexander.polyanov@gmail.com>
2024-10-07 15:43:04 +0300

    feat: Improving performance of merge function

--- packages/impulse-ui-core/src/utils/deepmerge.ts
+++ packages/impulse-ui-core/src/utils/deepmerge.ts
@@ -1,33 +1,20 @@
 const isObject = (value: string | object | Function) => typeof value === 'object';
-const isPrimitive = (value: string | object | Function) => typeof value !== 'object' && typeof value !== 'function';
-const isFunction = (value: string | object | Function) => typeof value === 'function';
 
 const merge = <T, E>(target?: T, source?: E) => {
   if (!source) return target;
   if (!target) return source;
 
-  const result = {};
-  const targetKeys = Object.keys(target);
+  const result = { ...target };
   const sourceKeys = Object.keys(source);
-  let key;
-
-  for (let i = 0; i < targetKeys.length; i++) {
-    key = targetKeys[i];
-
-    if (key) {
-      if (isObject(target[key])) result[key] = merge(target[key], source[key]);
-      if (isPrimitive(target[key])) result[key] = target[key];
-      if (isFunction(target[key])) result[key] = target[key];
-    }
-  }
 
   for (let i = 0; i < sourceKeys.length; i++) {
-    key = sourceKeys[i];
+    let key = sourceKeys[i];
+    const sourceValue = source[key];
 
-    if (key) {
-      if (isObject(source[key])) result[key] = merge(target[key], source[key]);
-      if (isPrimitive(source[key])) result[key] = source[key];
-      if (isFunction(source[key])) result[key] = source[key];
+    if (isObject(sourceValue)) {
+      result[key] = merge(target[key], sourceValue);
+    } else {
+      result[key] = sourceValue;
     }
   }
 
